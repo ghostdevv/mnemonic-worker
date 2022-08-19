7443d57313433d188d475fb70c66b7f23beb3e96
GHOST <ghostdevbusiness@gmail.com>
2022-08-19 23:51:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Mnemonic Worker
+
+This worker generates mnemonic phrases. It uses the rust (mnemonic crate)[https://crates.io/crates/mnemonic] and (getrandom)[https://crates.io/crates/getrandom]
+
+# Routes
+
+## GET /new
+
+Returns an array of 24 words. For example:
+
+```
+GET /new
+
+[
+    "uniform",
+    "equal",
+    "olivia",
+    "rhino",
+    "orchid",
+    "herbert",
+    "toyota",
+    "flower",
+    "quality",
+    "table",
+    "plume",
+    "dublin",
+    "bali",
+    "store",
+    "dominic",
+    "lotus",
+    "camel",
+    "protein",
+    "carrot",
+    "plaza",
+    "speed",
+    "remark",
+    "pyramid",
+    "john"
+]
+```
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use regex::Regex;
+use serde_json::json;
+use worker::*;
+
+mod utils;
+
+fn get_random_buf() -> Result<[u8; 32]> {
+    let mut buf = [0u8; 32];
+    let _ = getrandom::getrandom(&mut buf);
+    Ok(buf)
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    utils::log_request(&req);
+    utils::set_panic_hook();
+
+    let router = Router::new();
+
+    router
+        // .get("/", |_, _| {
+        //     Response::redirect(Url::from("https://github.com/ghostdevv/mnemonic-worker"))
+        // })
+        .get("/new", |_, _| {
+            let bytes = get_random_buf().unwrap();
+            let result = mnemonic::to_string(&bytes);
+
+            let words: Vec<String> = Regex::new("(-)+")
+                .unwrap()
+                .split(&result)
+                .map(|x| x.to_string())
+                .collect();
+
+            return Response::from_json(&json!(words));
+        })
+        .run(req, env)
+        .await
+}
